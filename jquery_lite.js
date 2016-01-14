f7fe37e5652e1e8758fabfe4c2dabe2fa94fa93d
Rafael <rafiepatel@gmail.com>
2016-01-14 11:33:00 -0500

    Allow core function to take HTMLElement(s) and CSS selectors.

--- jquery_lite.js
+++ jquery_lite.js
@@ -1,9 +1,18 @@
 (function () {
   // All the code!
   window.$l = function (arg) {
-    var nodeList = document.querySelectorAll(arg);
-    nodeList = [].slice.call(nodeList);
-    return new DOMNodeCollection(nodeList);
+    var collection;
+    if (arg instanceof HTMLElement) {
+      var argArray = [arg];
+      collection = new DOMNodeCollection(argArray);
+    } else if (arg instanceof Array) {
+      collection = new DOMNodeCollection(arg);
+    } else {
+      var nodeList = document.querySelectorAll(arg);
+      nodeList = [].slice.call(nodeList);
+      collection = new DOMNodeCollection(nodeList);
+    }
+    return collection;
   };
 
   var DOMNodeCollection = function (array) {
